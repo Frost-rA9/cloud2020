0aff7fb37847ab81dc4b714dcb58288e63dd4e04
霜暮雪 <Frost.rA9@gmail.com>
2023-05-20 15:09:20 +0800

    refactor : rename cloud-provider-payment8002 main entry

--- cloud-provider-payment8002/src/main/java/org/droplet/springcloud/PaymentMain8002.java
+++ cloud-provider-payment8002/src/main/java/org/droplet/springcloud/PaymentMain8002.java
@@ -6,8 +6,8 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class Payment8002Main {
+public class PaymentMain8002 {
     public static void main(String[] args) {
-        SpringApplication.run(Payment8002Main.class, args);
+        SpringApplication.run(PaymentMain8002.class, args);
     }
 }
