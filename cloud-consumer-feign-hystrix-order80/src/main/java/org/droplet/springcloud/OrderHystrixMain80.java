165c05b59b8d59bc1ebcf1a6b7cb6f16a140c84e
霜暮雪 <Frost.rA9@gmail.com>
2023-05-26 17:23:01 +0800

    feat : hystrix build

---
+++ cloud-consumer-feign-hystrix-order80/src/main/java/org/droplet/springcloud/OrderHystrixMain80.java
@@ -0,0 +1,11 @@
+package org.droplet.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderHystrixMain80 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderHystrixMain80.class, args);
+    }
+}
---
+++ cloud-provider-hystrix-payment8001/src/main/java/org/droplet/springcloud/service/PaymentService.java
@@ -0,0 +1,22 @@
+package org.droplet.springcloud.service;
+
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.TimeUnit;
+
+@Service
+public class PaymentService {
+    public String paymentInfo_OK(Integer id) {
+        return "线程池:" + Thread.currentThread().getName() + "paymentInfo_OK,id:" + id;
+    }
+
+    public String paymentInfo_TimeOut(Integer id) {
+        int timeNumber = 5;
+        try {
+            TimeUnit.SECONDS.sleep(timeNumber);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return "线程池:" + Thread.currentThread().getName() + "paymentInfo_TimeOut,id:" + id + "耗时(秒)" + timeNumber ;
+    }
+}
