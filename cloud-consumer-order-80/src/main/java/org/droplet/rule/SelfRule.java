5b5705b516e795d6bbe3c17b99ec2ecf934dcd7f
霜暮雪 <Frost.rA9@gmail.com>
2023-05-26 16:34:49 +0800

    fix : fix feign && ribbon

--- cloud-consumer-order-80/src/main/java/org/droplet/rule/SelfRule.java
+++ cloud-consumer-order-80/src/main/java/org/droplet/rule/SelfRule.java
@@ -1,16 +1,16 @@
 package org.droplet.rule;
 
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-// Spring Cloud 版本太新没有 Ribbon
 public class SelfRule {
-    /*
+
     @Bean
-    public IRule rule()
-    {
+    public IRule rule() {
         // 随机规则
         return new RandomRule();
     }
-    */
 }
