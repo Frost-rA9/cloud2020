83b23257b7521e36365b938a17c1025c7e0c2bc2
霜暮雪 <Frost.rA9@gmail.com>
2023-05-26 10:57:13 +0800

    feat : restemplate get for entity

--- cloud-consumer-order-80/src/main/java/org/droplet/springcloud/controller/OrderController.java
+++ cloud-consumer-order-80/src/main/java/org/droplet/springcloud/controller/OrderController.java
@@ -3,6 +3,7 @@ package org.droplet.springcloud.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.droplet.springcloud.entities.CommonResult;
 import org.droplet.springcloud.entities.Payment;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,5 +28,15 @@ public class OrderController {
     @GetMapping("/consumer/payment/get/{id}")
     public CommonResult<Payment> getPayment(@PathVariable("id") Long id) {
         return restTemplate.getForObject(PAYMENT_URL + "/payment/get/" + id, CommonResult.class);
+    }
+
+    @GetMapping("/consumer/payment/entity/get/{id}")
+    public CommonResult<Payment> getEntityPayment(@PathVariable("id") Long id) {
+        ResponseEntity<CommonResult> entity = restTemplate.getForEntity(PAYMENT_URL + "/payment/get/" + id, CommonResult.class);
+        if (entity.getStatusCode().is2xxSuccessful()) {
+            return entity.getBody();
+        } else {
+            return new CommonResult<>(444, "操作失败");
+        }
     }
 }
